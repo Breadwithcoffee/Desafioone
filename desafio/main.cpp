933ccd045b04c19260b4c983b14fc39b0d27c3db
Mateo <matteorodriguezyepez18@gmail.com>
2024-09-14 11:33:50 -0500

    organizando y planteando el lector de datos segundos a segundos

--- desafio/main.cpp
+++ desafio/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
-int main()
+// el codigo que lee los datos de la señal
+
+/*
+int analogPing = 0;
+int val = 0;
+
+void setup()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  Serial.begin(9600);
 }
+void loop()
+{
+  val = analogRead(analogPing);
+  Serial.println(val);
+}
+*/
